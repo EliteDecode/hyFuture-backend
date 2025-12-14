3e6930bc4a8e091571d6cda2d3c0ca513df7f9b2
Gospel Jonathan <gospyjo@gmail.com>
2025-12-14 23:56:51 +0100

    update: updated mail

--- src/common/utils/email.utils.ts
+++ src/common/utils/email.utils.ts
@@ -6,20 +6,39 @@ interface MailOptions {
   html: string;
 }
 
+// const sendMail = async ({ to, subject, html }: MailOptions): Promise<void> => {
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     host: 'smtp.gmail.com',
+//     port: 587,
+//     secure: false, // true for 465, false for other ports
+//     auth: {
+//       user: process.env.EMAIL_APP_DEV,
+//       pass: process.env.EMAIL_APP_PASSWORD_DEV,
+//     },
+//   });
+
+//   await transporter.sendMail({
+//     from: process.env.EMAIL_APP_DEV,
+//     to: to,
+//     subject: subject,
+//     html: html,
+//   });
+// };
+
 const sendMail = async ({ to, subject, html }: MailOptions): Promise<void> => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    host: 'mail.privateemail.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
-      user: process.env.EMAIL_APP_DEV,
-      pass: process.env.EMAIL_APP_PASSWORD_DEV,
+      user: process.env.EMAIL_SMTP,
+      pass: process.env.EMAIL_SMTP_PASSWORD,
     },
   });
 
   await transporter.sendMail({
-    from: process.env.EMAIL_APP_DEV,
+    from: process.env.EMAIL_SMTP,
     to: to,
     subject: subject,
     html: html,
